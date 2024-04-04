4f6e21966be21029c43e98696f3fe88876cc0309
Artem V <a.vitkouski@gmail.com>
2024-04-04 16:21:07 +0300

    Install glibc 2.29

--- installnode.sh
+++ installnode.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+wget -c https://ftp.gnu.org/gnu/glibc/glibc-2.29.tar.gz
+tar -zxvf glibc-2.29.tar.gz
+mkdir glibc-2.29/build
+cd glibc-2.29/build
+../configure --prefix=/opt/glibc
+make 
+make install
+cd ..
 sudo apt-get update && sudo apt-get -y upgrade
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
